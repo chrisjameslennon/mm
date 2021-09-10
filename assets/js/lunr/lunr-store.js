a79d0a4375c653359a561e55d05180833f96cf64
Chris Lennon <36693484+chrisjameslennon@users.noreply.github.com>
2021-09-11 10:23:32 +1200

    revert

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -22,14 +22,6 @@ var store = [
         "title": {{ doc.title | jsonify }},
         "excerpt":
           {%- if site.search_full_content == true -%}
-                {%- if doc.excerpt -%}
-                
-                   {{ doc.excerpt | newline_to_br | strip_newlines }},
-                
-                {%- endif -%}
-
-
-                {%- unless doc.excerpt -%}
             {{ doc.content | newline_to_br |
               replace:"<br />", " " |
               replace:"</p>", " " |
@@ -51,7 +43,6 @@ var store = [
               replace:"</h5>", " " |
               replace:"</h6>", " "|
             strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-          {%- endunless -%}
           {%- endif -%}
         "categories": {{ doc.categories | jsonify }},
         "tags": {{ doc.tags | jsonify }},
